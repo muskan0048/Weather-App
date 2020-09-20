418e1d627bbca8453e08bff90f9976f4f17b0402
muskan0048 <muskan1998>
2020-09-20 15:02:02 +0530

    new commt

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/login" component={Login} />
-    <Route exact path="/:name" component={Day} />
+    <Route exact path="/day" component={Day} />
    </Switch> 
   </BrowserRouter>
   );
