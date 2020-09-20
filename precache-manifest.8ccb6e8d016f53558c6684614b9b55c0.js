0f44e2b61df84868a39880182852c02b7ae0134b
muskan0048 <muskan1998>
2020-09-20 15:09:15 +0530

    Updates

---
+++ precache-manifest.8ccb6e8d016f53558c6684614b9b55c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3cc730ad7681b9da5cf933067ade22",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b41583da6da24a0c117",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2a6e77b7cbc1de29a669",
+    "url": "./static/css/main.fc7ce0ff.chunk.css"
+  },
+  {
+    "revision": "4b41583da6da24a0c117",
+    "url": "./static/js/2.a3674401.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.a3674401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6e77b7cbc1de29a669",
+    "url": "./static/js/main.422a5367.chunk.js"
+  },
+  {
+    "revision": "5acfd12ba8d6c9ddfbe3",
+    "url": "./static/js/runtime-main.94c40845.js"
+  },
+  {
+    "revision": "d59c8501d9a3fb511275d6da6cb7fee7",
+    "url": "./static/media/cloud-back.d59c8501.jpeg"
+  },
+  {
+    "revision": "958c94293dde0715355db09161a12ff7",
+    "url": "./static/media/mountain.958c9429.jpg"
+  },
+  {
+    "revision": "aa0d4d419eb7f922f5566b927bd6139d",
+    "url": "./static/media/night-back.aa0d4d41.jpg"
+  },
+  {
+    "revision": "27271186b76fcbd08cc0536ac2cd9fd6",
+    "url": "./static/media/rain-back.27271186.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Zelish-Assignment/precache-manifest.293c9f200a99a3ecc3b62a75ee99c152.js"
+  "./precache-manifest.8ccb6e8d016f53558c6684614b9b55c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Zelish-Assignment/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
